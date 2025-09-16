d7b7a2cdf68f361d7986e0c70d2edad4ae3b29d5
Duc Minh Nguyen <2476537@edu.vaniercollege.qc.ca>
2025-09-16 09:45:30 -0400

    first three question of assignement finished, 4th started

--- Assignment1.py
+++ Assignment1.py
@@ -1,26 +1,37 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    maximum = max(num1,num2,num3)
+
+    return maximum
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    minimum = min(num1, num2,num3)
+
+    return minimum
 
 # Function 3: Conditional Statements – The If Statement
 # This function should check if a number is positive, negative, or zero and return the corresponding message.
 def check_number(number):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    if number > 0:
+        
+        return("Positive")
+    elif number < 0:
+        
+        return("Negative")
+    else:
+        
+        return("Zero")
 
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
 def star_shape(rows):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    rows = {"******"}
+    
+    for char in range(rows):
+        return char
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
@@ -33,3 +44,5 @@ def count_multiples_of_3(limit):
 def sum_of_even_numbers(start, end):
     # TODO: Implement this function
     pass  # Replace with your code
+
+print(star_shape(5))
