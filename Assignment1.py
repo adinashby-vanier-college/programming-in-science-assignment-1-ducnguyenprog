3db6a789f84ee35977118158b05a7cc5334db8f3
Duc Minh Nguyen <2476537@edu.vaniercollege.qc.ca>
2025-09-23 09:45:37 -0400

    first three completed

--- Assignment1.py
+++ Assignment1.py
@@ -1,14 +1,14 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    maximum = max(num1,num2,num3)
+    maximum = max(num1, num2, num3)
 
     return maximum
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    minimum = min(num1, num2,num3)
+    minimum = min(num1, num2, num3)
 
     return minimum
 
@@ -28,20 +28,22 @@ def check_number(number):
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
 def star_shape(rows):
-    for x in range(1,rows+1):
-        print ("*" * x)
+    for x in range(1, rows + 1):
+        
+
+       return
         
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
 def count_multiples_of_3(limit):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    print 
+
+
 
 # Function 6: Sum of Even Numbers in a Range
 # This function should calculate and return the sum of even numbers within a given range.
 def sum_of_even_numbers(start, end):
-    # TODO: Implement this function
-    pass  # Replace with your code
-
+    print
+    
 
